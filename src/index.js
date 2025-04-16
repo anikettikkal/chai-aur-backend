339f185d0207f63fcf05725741f2a9e75c02f2b2
anikettikkal <tikkalaniket2019@gmail.com>
2025-04-16 22:27:12 +0530

    api error and asyncHandler function import

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("Error connecting to MONGO_DB");
+    
+})
 
 /*
 import express from "express";
