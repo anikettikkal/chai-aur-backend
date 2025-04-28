f86fc3e367f0d7608e5b6b82f8870a97b56fd69b
anikettikkal <tikkalaniket2019@gmail.com>
2025-04-28 23:14:22 +0530

    cloudinary file CREATED AND STARTING TO CREATE FILE UPLOAD

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,1 +1,8 @@
-import { v2 } from "cloudinary";
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME, // Click 'View API Keys' above to copy your cloud name, 
+    api_key: process.env.CLOUDINARY_API_KEY, // Click 'View API Keys' above to copy your API key
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
