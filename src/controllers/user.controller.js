ef0db0d40d7468d5b06c20b35cfd5b059918c76c
anikettikkal <tikkalaniket2019@gmail.com>
2025-05-11 22:14:40 +0530

    user data req and res created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,30 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
 
 
 const registerUser = asyncHandler( async (req, res)=>{
-    res.status(200).json({
-        message: "ok"
-    })
+   // get user details from frontend
+   // validation - not empty
+   // check if user already exists - email or username
+   // check for images- check for avtar
+   // upload them to cloudinary, avatar
+   // create user object - create entry in db
+   // remove password and refresh token field from response
+   // check for user creation
+   // return response
+
+
+   const {fullname, email, username, password} = req.body
+   console.log("email", email);
+   
+   if (
+    [fullname,email,username,password].some((field)=>field.trim() === "")
+) 
+{
+    throw new ApiError(400, "All fields are required")
+    
+}
+
 })
 
 
